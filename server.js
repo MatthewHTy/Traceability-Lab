2d075fc2b04aed53936ae3baff26dffa1312d960
Matthew Ty <matthewty1220@gmail.com>
2021-08-31 13:58:33 -0600

    added changes to rollbar

--- server.js
+++ server.js
@@ -1,11 +1,19 @@
 const express = require('express')
 const path = require('path')
+const Rollbar = require('rollbar')
 
+let rollbar = new Rollbar ({
+    accesToken: '50e4a655b9f64093934060fb75eaa349',
+    captureUncaught: true,
+    captureUnhandledRejections: true
+})
 const app = express()
 
 app.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '/public/index.html'))
+    rollbar.info('html file server successfully.')
 })
+
 
 const port = process.env.PORT || 4545
 
